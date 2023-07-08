2adbc480b0d421868a9a246cf851334691ae81bf
vapb <vapb7123@gmail.com>
2023-07-08 09:14:11 -0300

    Add sequence exercise

--- javascript-youtube/exercise-6/script.js
+++ javascript-youtube/exercise-6/script.js
@@ -1,9 +1,34 @@
+var title_seq = document.getElementById("title_seq")
+var result_seq = document.getElementById("result_seq")
 var generate = document.getElementById('generate')
 generate.addEventListener('click', generate_sequence)
 
 function generate_sequence() {
+    var sequence = ""
     var start = Number(document.getElementById('start_number').value);
     var end = Number(document.getElementById('end_number').value);
     var step = Number(document.getElementById('step').value);
     console.log(`Start : ${start} - End : ${end} - Step : ${step}`)
+
+    if ((end <= start) | (step <= 0)) {
+        title_seq.innerHTML = "<span style='color:red';> No sequence available </span>"
+        sequence = " ... "
+    }
+    
+    else {
+        for (var i = start; i < end; i = i + step) {
+            sequence = sequence + `${i} - `
+        }
+            if ((end - start) % step != 0) {
+                title_seq.innerHTML = "<span style='color:orange';>Incomplete sequence</span>" 
+                sequence += `X`
+            }
+            else {
+                title_seq.innerHTML = "<span style='color:green';>Complete sequence</span>"
+                sequence += `${end}`
+            }
+    }
+
+    result_seq.innerText = sequence
 }
+

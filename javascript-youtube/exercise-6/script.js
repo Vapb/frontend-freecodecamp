5e4e4dddf58cd8cf024bafba20aa2472b95de776
vapb <vapb7123@gmail.com>
2023-07-08 14:15:41 -0300

    refactor: sequence-exercise

--- javascript-youtube/exercise-6/script.js
+++ javascript-youtube/exercise-6/script.js
@@ -1,53 +1,45 @@
-var title_seq = document.getElementById("title_seq")
-var result_seq = document.getElementById("result_seq")
-var generate = document.getElementById('generate')
-generate.addEventListener('click', generate_sequence)
+const titleSeq = document.getElementById("title_seq");
+const resultSeq = document.getElementById("result_seq");
+const generate = document.getElementById('generate');
+generate.addEventListener('click', generateSequence);
 
-function generate_sequence() {
-    var sequence = ""
+function calcSequence(start, end, step, direction){
+    let sequence = []
+    for (let i = start; direction * i < direction * end; i += step) {
+        console.log(`${direction > 0 ? 'CRES' : 'DESC'} ${i}`);
+        sequence.push(i);
+    }
+
+    if ((end - start) % step !== 0) {
+        titleSeq.innerHTML = "<span style='color:orange;'>Incomplete sequence</span>";
+        sequence.push('X');
+    } else {
+        titleSeq.innerHTML = "<span style='color:green;'>Complete sequence</span>";
+        sequence.push(end);
+    }
+
+    return sequence
+}
+
+
+function generateSequence() {
+    var sequence = []
     var start = Number(document.getElementById('start_number').value);
     var end = Number(document.getElementById('end_number').value);
     var step = Number(document.getElementById('step').value);
     console.log(`Start : ${start} - End : ${end} - Step : ${step}`)
     
-    // Cres count
-    if ((step > 0) && (end > start)) {
-        console.log('CRES')
-        for (var i = start; i < end; i = i + step) {
-            sequence = sequence + `${i} - `
-        }
-            if ((end - start) % step != 0) {
-                title_seq.innerHTML = "<span style='color:orange';>Incomplete sequence</span>" 
-                sequence += `X`
-            }
-            else {
-                title_seq.innerHTML = "<span style='color:green';>Complete sequence</span>"
-                sequence += `${end}`
-            }
+    if (step > 0 && end > start) {
+        console.log('CRES');
+        sequence = calcSequence(start, end, step, 1)
+    } else if (step < 0 && end < start) {
+        console.log('DESC');
+        sequence = calcSequence(start, end, step, -1)
+    } else {
+        titleSeq.innerHTML = "<span style='color:red;'>No sequence available</span>";
+        resultSeq.innerText = " ... ";
     }
-    
-    // Desc count
-    else if ((step < 0) && (end < start)){
-        console.log('DESC')
-        for (var i = start; i > end; i = i + step) {
-            console.log(`DESC ${i}`)
-            sequence = sequence + `${i} - `
-        }
-            if ((end - start) % step != 0) {
-                title_seq.innerHTML = "<span style='color:orange';>Incomplete sequence</span>" 
-                sequence += `X`
-            }
-            else {
-                title_seq.innerHTML = "<span style='color:green';>Complete sequence</span>"
-                sequence += `${end}`
-            }
-    }
-    
-    // start == end | step zero
-    else {
-        title_seq.innerHTML = "<span style='color:red';> No sequence available </span>"
-        sequence = " ... "
-    }
-    result_seq.innerText = sequence
-}
 
+    resultSeq.innerText = sequence.join(' - ');
+    return;
+}

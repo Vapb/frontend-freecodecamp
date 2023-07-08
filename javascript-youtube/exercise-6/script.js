759b55902294efb534596eb12f6866f4868e17de
vapb <vapb7123@gmail.com>
2023-07-08 14:18:33 -0300

    remove unnecessary prints

--- javascript-youtube/exercise-6/script.js
+++ javascript-youtube/exercise-6/script.js
@@ -6,7 +6,6 @@ generate.addEventListener('click', generateSequence);
 function calcSequence(start, end, step, direction){
     let sequence = []
     for (let i = start; direction * i < direction * end; i += step) {
-        console.log(`${direction > 0 ? 'CRES' : 'DESC'} ${i}`);
         sequence.push(i);
     }
 
@@ -27,13 +26,10 @@ function generateSequence() {
     var start = Number(document.getElementById('start_number').value);
     var end = Number(document.getElementById('end_number').value);
     var step = Number(document.getElementById('step').value);
-    console.log(`Start : ${start} - End : ${end} - Step : ${step}`)
     
     if (step > 0 && end > start) {
-        console.log('CRES');
         sequence = calcSequence(start, end, step, 1)
     } else if (step < 0 && end < start) {
-        console.log('DESC');
         sequence = calcSequence(start, end, step, -1)
     } else {
         titleSeq.innerHTML = "<span style='color:red;'>No sequence available</span>";

7bbc45e9badcb4d421a56275a098d6439bcde96e
vapb <vapb7123@gmail.com>
2023-07-12 21:58:19 -0300

    Complete exercise-8

--- javascript-youtube/exercise-8/script.js
+++ javascript-youtube/exercise-8/script.js
@@ -5,6 +5,14 @@ summaryBt.addEventListener("click", generateSummary)
 const resetBt = document.getElementById("reset_array")
 resetBt.addEventListener("click", wipeArray)
 
+const count_value = document.getElementById("count_td")
+const mean_value = document.getElementById("mean_td")
+const median_value = document.getElementById("median_td")
+const min_value = document.getElementById("min_td")
+const max_value = document.getElementById("max_td")
+const std_value = document.getElementById("std_td")
+let arraySelect = document.getElementById("array_select")
+
 let array = []
 
 function addNumber() {
@@ -12,48 +20,53 @@ function addNumber() {
     if (number.value.length == 0) {
         window.alert('Add number')
     } else {
-        console.log(Number(number.value))
-        array.push(Number(number.value))
+        number_value = Number(number.value)
+        let arrayOption = document.createElement('option')
+        arrayOption.innerText = number_value
+        arraySelect.appendChild(arrayOption)
+        
+        array.push(number_value)
     }
-}
-
-function generateSummary() {
-    console.log(array)
-    let len = array.length
-    let mean = array.length
-    let median = array.length
-    let std = array
-    let min = Math.min(...array)
-    let max = Math.max(...array)
-    console.log(len, min, max)
-
-    return
 }
 
 function wipeArray() {
     array = []
-    console.log(`array wiped ${array}`)
+    arraySelect.innerHTML = ""
 }
 
-// TODO std
+function generateSummary() {
+    console.log(array)
+    count_value.innerText = array.length
+    mean_value.innerText = meanArray(array)
+    median_value.innerText = medianArray(array)
+    std_value.innerText = standardDeviation(array)
+    max_value.innerText = Math.min(...array)
+    min_value.innerText = Math.max(...array)
+}
+
+
 function standardDeviation(arr) {
-
+    let mean = meanArray(arr)
+    let variance = 0 
+    for (let i = 0; i < arr.length; i++) {
+        variance += (mean - arr[i]) * (mean - arr[i])
+    }
+    variance = variance/arr.length
+    return Math.sqrt(variance)
 }
 
-// TODO Continue
-function median(arr) {
-    //sort
+function medianArray(arr) {
     arr.sort((a, b) => a - b)
-    let lenght = arr.lenght
-    if (lenght % 2 == 0) {
-
+    let length = arr.length
+    if (length % 2 == 0) {
+        return (arr[length/2] + arr[length/2-1])/2
     } 
     else {
-        return arr[lenght / 2]
+        return arr[Math.floor(length / 2)]
     }
 }
 
-function mean(arr) {
+function meanArray(arr) {
     let sum = 0
     arr.forEach(element => {
         sum += element
